04c02ec9e05cf1d1ee06639d0212bfc58d3487da
7jjin <wlsgud6305@naver.com>
2023-05-28 11:03:50 +0900

    배포 준비

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -12,6 +12,10 @@ const Diary = () => {
   const diaryList = useContext(DiaryStateContext);
   const navigate = useNavigate();
   const [data, setData] = useState();
+  useEffect(() => {
+    const titleElement = document.getElementsByTagName("title")[0];
+    titleElement.innerHTML = `감정 일기장 - ${id}번 일기`;
+  });
 
   useEffect(() => {
     const titleElement = document.getElementsByTagName("title")[0];
--- src/pages/New.js
+++ src/pages/New.js
@@ -1,6 +1,10 @@
 import DiaryEditor from "../components/DiaryEditor";
-
+import { useEffect } from "react";
 const New = () => {
+  useEffect(() => {
+    const titleElement = document.getElementsByTagName("title")[0];
+    titleElement.innerHTML = `감정 일기장 - 새일기`;
+  });
   return (
     <div>
       <DiaryEditor />
